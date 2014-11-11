b1ca0015b41c1c47cc4ea01e0a5dead50997f005
fifi <aVimXhuVluyt>
2014-11-11 20:52:24 +0100

    Ajout des champs prenom et nom lors de l'inscription

--- src/Countrynside/UserBundle/Entity/User.php
+++ src/Countrynside/UserBundle/Entity/User.php
@@ -18,4 +18,73 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    
+    
+    /**
+     * @ORM\Column(name="prenom",type="string")
+     * 
+     */
+    protected $prenom;
+    
+    /**
+     * @ORM\Column(name="nom",type="string")
+     * 
+     */
+    protected $nom;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     * @return User
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string 
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     * @return User
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
 }
