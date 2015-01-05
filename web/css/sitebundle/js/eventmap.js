c24a3ad0004616ce00b38419874e5ef3ee88ea9d
Bernacer <nacer.b13@gmail.com>
2015-01-05 10:36:14 +0100

    fix bug map & center image

--- web/css/sitebundle/js/eventmap.js
+++ web/css/sitebundle/js/eventmap.js
@@ -1,19 +1,20 @@
 var map;
-var myCenter=new google.maps.LatLng(53, -1.33);
+var myCenter=new google.maps.LatLng(46.6302142, 2.5680160)
 var marker=new google.maps.Marker({
     position:myCenter
 });
 
 function initialize() {
-    var mapOptions = {
+    var mapProp = {
+        center:myCenter,
+        draggable: false,
+        scrollwheel: true,
+        mapTypeId:google.maps.MapTypeId.ROADMAP,
         zoom: 6,
-        disableDefaultUI: true,
-        scrollwheel: false,
-        center: new google.maps.LatLng(46.6302142, 2.5680160)
+        disableDefaultUI: true
     };
 
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+    map=new google.maps.Map(document.getElementById("map-canvas"),mapProp);
     marker.setMap(map);
 
     google.maps.event.addListener(marker, 'click', function() {
@@ -27,7 +28,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 google.maps.event.addDomListener(window, "resize", resizingMap());
 
-$('#mapModal').on('shown', function() {
+$('#mapModal').on('show.bs.modal', function() {
     //Must wait until the render of the modal appear, thats why we use the resizeMap and NOT resizingMap!! ;-)
     resizeMap();
 })
@@ -49,7 +50,7 @@ function reinitialize(Coord) {
     var coord = res.split(", ")
     var pos = new google.maps.LatLng(coord[0], coord[1]);
     var mapOptions = {
-        zoom: 8,
+        zoom: 14,
         disableDefaultUI: true,
         scrollwheel: true,
         center: new google.maps.LatLng(coord[0], coord[1])
